a69ab9e2358e619c09f14c39f3d6adf507ec4aa7
Franklin Ramos <franklinramos@outlook.com>
2024-02-12 18:55:59 -0300

    Add new note card and update app layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,29 @@
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 export function App() {
   return (
-    <div className="">
-      <h1 className="underline text-3xl font-bold">Hello world!</h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <form className="w-full">
+        <input
+          type="text"
+          placeholder="Busque em suas notas..."
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-state-500"
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+        <NewNoteCard />
+
+        <NoteCard
+          note={{
+            date: new Date(),
+            content: 'Hello World',
+          }}
+        />
+      </div>
     </div>
   )
 }
